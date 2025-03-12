105dc37d1651f494b8e8842537b7e6465e4a550c
Dracocybertech <pointdujourkh@gmail.com>
2025-03-12 18:45:12 +0100

    Add test for supplyElectricityBlock

--- test/com/power_station/offer/ElectricityBlockTest.java
+++ test/com/power_station/offer/ElectricityBlockTest.java
@@ -1,6 +1,8 @@
 package test.com.power_station.offer;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.power_station.offer.ElectricityBlock;
 
@@ -21,5 +23,23 @@ public class ElectricityBlockTest {
         electricityBlockSupplied = new ElectricityBlock(energy, price, hours);
         electricityBlockNotSupplied = new ElectricityBlock(energy + 10, price + 10, 1);
         electricityBlockSuppliedLessHours = new ElectricityBlock(energy, price, 1);
+    }
+
+    @Test
+    public void supplyElectricityBlockTest() {
+        //Same block
+        Assert.assertTrue(electricityBlock.supplyElectricityBlock(electricityBlock));
+        
+        //Different blocks with the same settings
+        Assert.assertTrue(electricityBlock.supplyElectricityBlock(electricityBlockSupplied));
+        Assert.assertTrue(electricityBlockSupplied.supplyElectricityBlock(electricityBlock));
+        
+        //Blocks that can't supply each other because of energy difference
+        Assert.assertFalse(electricityBlock.supplyElectricityBlock(electricityBlockNotSupplied));
+        Assert.assertFalse(electricityBlockNotSupplied.supplyElectricityBlock(electricityBlock));
+    
+        //Blocks with different hours so one can supply the other but not the opposite
+        Assert.assertFalse(electricityBlockSuppliedLessHours.supplyElectricityBlock(electricityBlock));
+        Assert.assertTrue(electricityBlock.supplyElectricityBlock(electricityBlockSuppliedLessHours));
     }
 }
