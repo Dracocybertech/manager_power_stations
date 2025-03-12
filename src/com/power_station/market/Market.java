7082cfd3f0ff972efcea7cf6cdcb7d0957fd9796
Dracocybertech <pointdujourkh@gmail.com>
2025-03-12 17:09:26 +0100

    WIP getListParkOffer

--- src/com/power_station/market/Market.java
+++ src/com/power_station/market/Market.java
@@ -1,10 +1,14 @@
 package com.power_station.market;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.power_station.offer.ElectricityBlock;
 import com.power_station.offer.ElectricityOffer;
+import com.power_station.park.Park;
 
 public class Market {
 
@@ -61,4 +65,39 @@ public class Market {
         return Market.mapMarkets.values();
     }
 
+    /**
+     * Return the list of the parks which can produced electricity for this market.
+     * Return an empty list if there isn't any park or if there isn't any
+     * configuration of parks to supply the market.
+     * 
+     * @return
+     */
+    public List<Park> getListParkOffer() {
+        ArrayList<Park> listParksResult = new ArrayList<>();
+        List<Park> listParks = new ArrayList<>(Park.getParks());
+        if (this.getElectricityOffer() == null || listParks.isEmpty()) {
+            return listParksResult;
+        }
+
+        // Get all the electricity blocks we need to supply for this offer
+        List<ElectricityBlock> offerBlocks = this.getElectricityOffer().getElectricityBlocks();
+
+        // For each electricity block, we see if there is a park which can supply it
+        for (ElectricityBlock block : offerBlocks) {
+            ArrayList<Park> listParkBlock = new ArrayList<>();
+            for (Park park : listParks) {
+                if (park.supplyElectricityBlock(block)) {
+                    listParkBlock.add(park);
+                }
+                // There isn't any park that can provide for one of the electricity block
+                if (listParkBlock.isEmpty()) {
+                    return new ArrayList<>();
+                }
+                //Add the parks to the result lists
+                listParksResult.addAll(listParkBlock);
+            }
+        }
+
+        return listParksResult;
+    }
 }
