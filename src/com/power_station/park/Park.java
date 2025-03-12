fa276ffa410327602877fef1822671b6cff31ff2
Dracocybertech <pointdujourkh@gmail.com>
2025-03-12 16:12:15 +0100

    Init Park

---
+++ src/com/power_station/park/Park.java
@@ -0,0 +1,41 @@
+package com.power_station.park;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.power_station.offer.ElectricityBlock;
+
+public class Park {
+
+    private String type;
+    private String name;
+    private List<ElectricityBlock> electricityBlocks;
+    private static Map<String, Park> mapParks;
+
+    /**
+     * Create Park with empty production blocks.
+     * 
+     * @param type
+     * @param name
+     */
+    public Park(String type, String name) {
+        this(type, name, null);
+    }
+
+    /**
+     * Create a Park with supplied production blocks.
+     * 
+     * @param type
+     * @param name
+     */
+    public Park(String type, String name, List<ElectricityBlock> electricityBlocks) {
+        if (mapParks.containsKey(name)) {
+            throw new ParkAlreadyExistingException("Park with this name already exists.");
+        }
+        this.name = name;
+        this.type = type;
+        this.electricityBlocks = new ArrayList<>(electricityBlocks);
+        mapParks.put(name, this);
+    }
+}
