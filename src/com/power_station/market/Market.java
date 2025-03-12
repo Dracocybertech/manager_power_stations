4727c2540c1b8698012f2b588e7afe605dc39278
Dracocybertech <pointdujourkh@gmail.com>
2025-03-12 16:08:13 +0100

    Update name for the list of the markets

--- src/com/power_station/market/Market.java
+++ src/com/power_station/market/Market.java
@@ -12,7 +12,7 @@ public class Market {
 
     private String name;
     private List<ElectricityOffer> offers;
-    private static Map<String, Market> listmarkets = new HashMap<>();
+    private static Map<String, Market> mapMarkets = new HashMap<>();
 
     /**
      * Create a Market with a new name.
@@ -20,13 +20,13 @@ public class Market {
      * @param name
      */
     public Market(String name) {
-        if (listmarkets.containsKey(name)) {
+        if (mapMarkets.containsKey(name)) {
             throw new MarketAlreadyExistingException(
                     "The market with this name already exist. Please use another name.");
         }
         this.name = name;
         this.offers = new ArrayList<>();
-        listmarkets.put(name, this);
+        mapMarkets.put(name, this);
     }
 
     /**
@@ -36,13 +36,13 @@ public class Market {
      * @param offers
      */
     public Market(String name, List<ElectricityOffer> offers) {
-        if (listmarkets.containsKey(name)) {
+        if (mapMarkets.containsKey(name)) {
             throw new MarketAlreadyExistingException(
                     "The market with this name already exist. Please use another name.");
         }
         this.name = name;
         this.offers = new ArrayList<>(offers);
-        listmarkets.put(name, this);
+        mapMarkets.put(name, this);
     }
 
     /**
@@ -58,6 +58,6 @@ public class Market {
      * @return
      */
     public Collection<Market> getMarkets() {
-        return Market.listmarkets.values();
+        return Market.mapMarkets.values();
     }
 }
