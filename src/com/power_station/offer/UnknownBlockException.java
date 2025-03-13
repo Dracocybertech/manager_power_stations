03d60c8d96fe9fac5974339493c30081c249d361
Dracocybertech <pointdujourkh@gmail.com>
2025-03-13 15:13:23 +0100

    Switch to extends RuntimeException since it checked a RuntimeException from remove operation

--- src/com/power_station/offer/UnknownBlockException.java
+++ src/com/power_station/offer/UnknownBlockException.java
@@ -1,6 +1,6 @@
 package com.power_station.offer;
 
-public class UnknownBlockException extends Exception {
+public class UnknownBlockException extends RuntimeException {
     public UnknownBlockException() {
         super();
     }
