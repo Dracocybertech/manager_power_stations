e3ff23b87af8032b41c4d60b198bb42c83e0195c
Dracocybertech <pointdujourkh@gmail.com>
2025-03-13 15:47:28 +0100

    Init MarketTest

---
+++ test/com/power_station/market/MarketTest.java
@@ -0,0 +1,31 @@
+package test.com.power_station.market;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+
+import com.power_station.market.Market;
+import com.power_station.offer.ElectricityBlock;
+import com.power_station.offer.ElectricityOffer;
+
+public class MarketTest {
+
+    private Market emptyMarket;
+    private Market market;
+    private ElectricityOffer electricityOffer;
+    private ArrayList<ElectricityBlock> listElectricityBlocks;
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Market Test start");
+        emptyMarket = new Market("Primaire");
+        int energy = 10;
+        int price = 20;
+        int hours = 3;
+        ElectricityBlock electricityBlock = new ElectricityBlock(energy, price, hours);
+        listElectricityBlocks = new ArrayList<>();
+        listElectricityBlocks.add(electricityBlock);
+        electricityOffer = new ElectricityOffer(listElectricityBlocks);
+        market = new Market("Secondaire", electricityOffer);
+    }
+}
